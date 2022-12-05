7d31a4e170b11d7278c7374b035a57dd64a5d99f
Francesco P <44530535+frnprt@users.noreply.github.com>
2022-12-05 12:33:43 +0100

    REFACTORING: change application name.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Git Credentials Switcher
+# Git Profile Switcher
 ## Introduction
-Git Credentials Switcher (gitcs) is a little personal project to keep multiple ```user.name``` and ```user.email``` at hand. 
+Git Profile Switcher (gitps) is a little personal project to keep multiple ```user.name``` and ```user.email``` at hand. 
 
-It allows to instantly switch to a preset of git credentials without having to manually rewrite them every time.
+It allows to instantly switch to a preset of git profile without having to manually rewrite them every time.
 
-It is mainly targeted to those devs who prefer to have many different accounts/emails and want to use different credentials in commits depending on the origin domain.
+It is mainly targeted to those devs who prefer to have many different accounts/emails and want to use different profile in commits depending on the origin domain.
 The program has been made completely interactive via the Python [inquirer](https://github.com/magmax/python-inquirer) package.
 
 ## Installation
@@ -15,33 +15,33 @@ pip install inquirer
 * Download the repository via `git clone` or download as a zip.
 * Run the program:
 ```
-python3 path/to/git_credentials_switcher.py
+python3 path/to/git_profile_switcher.py
 ```
-This will also create a `.gitcs_config.ini` file in your home directory.
+This will also create a `.gitps_config.ini` file in your home directory.
 * Eventually, you can create an alias in your favourite shell. For instance, in `bash` you can execute the following command:
 ```
-echo "# Add alias for gitcs\nalias gitcs=\042python3 /path/to/GitCredentialsSwitcher/git_credentials_switcher.py\042" >> $HOME/.bashrc
+echo "# Add alias for gitps\nalias gitps=\042python3 /path/to/GitProfileSwitcher/git_profile_switcher.py\042" >> $HOME/.bashrc
 ```
 
 ## Usage
 During execution, the program will first show the currently set `user.name` and `user.email`.
 
 It will then ask the user to choose a course of action.
-- `Switch git credentials`: start the process to switch the current `user.name` and `user.email` with previously stored credentials. <br> If you have no credentials stored in the configuration, the program will abort execution.
-- `Setup new git credentials`: start the process to add new credentials to the program configuration. If you already have credentials for a given domain or a given entry in a domain, the program will ask you if you want to override them with the new ones.
-- `Erase git credentials`: start the process to erase an existing set of credentials from configuration. <br> If you have no credentials stored, the program will abort execution.
+- `Switch git profile`: starts the process to switch the current `user.name` and `user.email` with previously stored profile. <br> Those will be saved with the `--global` option. If you have no profiles stored in the configuration, the program will abort execution.
+- `Setup new git profile`: starts the process to add a new profile to the program configuration. If you already have profile for a given domain or a given entry in a domain, the program will ask you if you want to override them with the new ones.
+- `Erase git profile`: starts the process to erase an existing profile from configuration. <br> If you have no profile stored, the program will abort execution.
 
 ## Populating the configuration file
 The configuration file follows the classic `.ini` format (more on it [here](https://en.wikipedia.org//wiki/INI_file)).
 
-To add new credentials to the config you can either use the built-in helper or you can edit your `gitcs_config.ini` directly.
+To add a new profile to the config you can either use the built-in helper or you can edit your `gitps_config.ini` directly.
 
 ### 1) Using the built-in helper
-Assuming you have aliased the command `gitcs`:
-- Execute `gitcs`;
-- From the inquirer interface, choose `Setup new git credentials`;
-- Insert the domain name you want to associate the credentials to (e.g `github.com`);
-- Insert the name you want the entry to be saved with in the config file. <br> This can be any word you associate to that particular set of credentials;
+Assuming you have aliased the command `gitps`:
+- Execute `gitps`;
+- From the inquirer interface, choose `Setup new git profile`;
+- Insert the domain name you want to associate the profile to (e.g `github.com`);
+- Insert the name you want the entry to be saved with in the config file. <br> This can be any word you associate to that particular profile;
 - Insert your `user.name`;
 - Insert your `user.email`;
 
@@ -59,7 +59,7 @@ The interpretation of the file is as follows:
 
     In any case, most of the mail providers directly forbid the `"` character in the local part of the address.
 
-An example of correctly set `gitcs_config.ini` is:
+An example of correctly set `gitps_config.ini` is:
 ```
 [github.com]
 personal = myusername"myuseremail@example.com
@@ -68,8 +68,8 @@ university = myuniversityname"myuniversityemail@mail.com
 [anotherdomain.io]
 personal = anothername"anothermail@example.com
 ```
-## Erasing stored credentials
-You can either use the guided process via the `Erase git credentials` option or edit the `.ini` file directly.
+## Erasing stored profile
+You can either use the guided process via the `Erase git profile` option or edit the `.ini` file directly.
 
 It's worth noting that, if you delete an entry with the program built-in interface, it will also delete the corresponding domain section if the deleted entry was the only one associated with it.
 
--- git_profile_switcher.py
+++ git_profile_switcher.py
@@ -5,18 +5,18 @@ from pathlib import Path
 
 import inquirer
 
-# Show current credentials
+# Show current profile
 print()
-print("! Current credentials:")
+print("💾 Current profile:")
 os.system("echo USERNAME: `git config user.name`")
 os.system("echo EMAIL: `git config user.email`")
 print()
 # Ask for command
 questions = [
     inquirer.List('action',
-                  message="What do you need to do?",
-                  choices=['Switch git credentials',
-                           'Setup new git credentials', 'Erase git credentials'],
+                  message="❔ What do you need to do?",
+                  choices=['Switch git profile',
+                           'Setup new git profile', 'Erase git profile'],
                   ),
 ]
 answers = inquirer.prompt(questions)
@@ -25,22 +25,22 @@ action = answers["action"]
 # Initialize the parser for the persistent configuration file
 config = configparser.ConfigParser()
 prefix = home = str(Path.home())
-config.read(home + "/.gitcs_config.ini")
+config.read(home + "/.gitps_config.ini")
 domains = config.sections()
 
 # Take an action based on the previous answer
 match action:
-    case "Switch git credentials":
+    case "Switch git profile":
         # Check that there is at least one domain registered in the config
         if domains.__len__() == 0:
-            print("! You have no credentials stored.\n" +
-                  "! Add some with the \"Setup new git credentials\" option and retry")
+            print("⚠️ You have no profiles stored.\n" +
+                  "⚠️ Add some with the \"Setup new git profile\" option and retry")
             sys.exit(-1)
         else:
             # If there is one, ask which to browse
             questions = [
                 inquirer.List('domain',
-                              message="Select the domain you want to switch the credentials for:",
+                              message="❔ Select the domain you want to switch the profile for:",
                               choices=domains,
                               ),
             ]
@@ -48,13 +48,13 @@ match action:
             domain = answers['domain']
             # Check that the selected domain has at least one entry 
             if config[domain].__len__() == 0:
-                print("! You have no credentials stored for the selected domain.\n" +
-                      "! Add some with the \"Setup new git credentials\" option and retry")
+                print("⚠️ You have no profiles stored for the selected domain.\n" +
+                      "⚠️ Add some with the \"Setup new git profile\" option and retry")
                 sys.exit(-1)
             # If it does, ask which to write in .gitconfig
             questions = [
                 inquirer.List('entry',
-                              message="Select the git credentials to switch to:",
+                              message="❔ Select the git profile to switch to:",
                               choices=config[domain],
                               ),
             ]
@@ -65,35 +65,35 @@ match action:
             email = values[1]
             os.system("git config --global user.name \"" + name + "\"")
             os.system("git config --global user.email \"" + email + "\"")
-            print("Successfully switched credentials.")
+            print("✅ Successfully switched profile.")
             sys.exit(0)
 
-    case "Setup new git credentials":
+    case "Setup new git profile":
         # Ask for domain and entry name
-        print("! NOTE: Refrain from using the \" and = characters in any input field.")
+        print("⚠️ NOTE: Refrain from using the \" and = characters in any input field.")
         domain = input("Domain name [e.g. \"github.com\"]: ")
         if "=" in domain or "\"" in domain:
-                print("! The [\"=] characters are not allowed.\n" +
-                      "! Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
+                print("⚠️ The [\"=] characters are not allowed.\n" +
+                      "⚠️ Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
                 sys.exit(-1)
         entry = input("Entry name [e.g. \"university-account\"]: ")
         if "=" in entry or "\"" in entry:
-                print("! The [\"=] characters are not allowed.\n" +
-                      "! Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
+                print("⚠️ The [\"=] characters are not allowed.\n" +
+                      "⚠️ Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
                 sys.exit(-1)
         # If the domain already exists in the config:
         if config.has_section(domain):
-            print("! The domain already exists in the config.\n")
+            print("❕ The domain already exists in the config.\n")
             # Check if the entry exists too
             if config.has_option(domain, entry):
                 # If it does, ask to proceed with overwrite
-                print("! The selected domain already has an entry with the same name.\n")
-                overwrite = input("! Do you want to overwrite it [y/N]? ")
+                print("❕ The selected domain already has an entry with the same name.\n")
+                overwrite = input("❕ Do you want to overwrite it [y/N]? ")
                 # If not, abort the operation
                 if overwrite.strip() != "y":
-                    print("Aborting operation...")
+                    print("❌ Aborting operation...")
                     sys.exit(-1)
-            print("! The new credentials will be added to the existing domain...")
+            print("❕ The new profile will be added to the existing domain...")
         # Else, add it to the config
         else:
             config.add_section(domain)
@@ -101,30 +101,30 @@ match action:
         name = input("user.name: ")
         email = input("user.email: ")
         if "=" in name or "\"" in name:
-                print("! The [\"=] characters are not allowed.\n" +
-                      "! Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
+                print("⚠️ The [\"=] characters are not allowed.\n" +
+                      "⚠️ Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
                 sys.exit(-1)
         if "=" in email or "\"" in email:
-                print("! The [\"=] characters are not allowed.\n" +
-                      "! Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
+                print("⚠️ The [\"=] characters are not allowed.\n" +
+                      "⚠️ Please consider changing your username for the selected domain and/or changing/aliasing your email address.")
                 sys.exit(-1)
         config.set(domain, entry, name + "\"" + email)
-        with open(home + '/.gitcs_config.ini', 'w') as configfile:
+        with open(home + '/.gitps_config.ini', 'w') as configfile:
             config.write(configfile)
-            print("New credentials successfully stored.")
+            print("✅ New profile successfully stored.")
             sys.exit(0)
 
-    case "Erase git credentials":
+    case "Erase git profile":
         # Check that there is at least one domain registered in the config
         if domains.__len__() == 0:
-            print("! You have no credentials stored.\n" +
-                  "! Add some with the \"Setup new git credentials\" option and retry")
+            print("⚠️ You have no profiles stored.\n" +
+                  "⚠️ Add some with the \"Setup new git profile\" option and retry")
             sys.exit(-1)
         else:
             # If there is one, ask which to browse
             questions = [
                 inquirer.List('domain',
-                              message="Select the domain you want to switch the credentials for:",
+                              message="❔ Select the domain you want to switch the profile for:",
                               choices=domains,
                               ),
             ]
@@ -132,13 +132,13 @@ match action:
             domain = answers['domain']
             # Check that the selected domain has at least one entry
             if config[domain].__len__() == 0:
-                print("! You have no credentials stored for the selected domain.\n" +
-                      "! Add some with the \"Setup new git credentials\" option and retry")
+                print("⚠️ You have no profiles stored for the selected domain.\n" +
+                      "⚠️ Add some with the \"Setup new git profiles\" option and retry")
                 sys.exit(-1)
             # If it does, ask to select which is going to be deleted
             questions = [
                 inquirer.List('entry',
-                              message="Select the git credentials to switch to:",
+                              message="❔ Select the git profile to switch to:",
                               choices=config[domain],
                               ),
             ]
@@ -149,7 +149,7 @@ match action:
             # If that was the last entry of the domain, delete the domain
             if config[domain].__len__ == 0:
                 config.remove_section(domain)
-            with open(home + '/.gitcs_config.ini', 'w') as configfile:
+            with open(home + '/.gitps_config.ini', 'w') as configfile:
                 config.write(configfile)
-                print("Credentials successfully erased.")
+                print("✅ profile successfully erased.")
                 sys.exit(0)
